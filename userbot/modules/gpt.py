40163f443495d8e6f82db7abe67c8d331b351311
drdilyor <drdilyor@outlook.com>
2024-03-07 13:57:14 +0500

    Set mongo_db_uri

--- userbot/modules/gpt.py
+++ userbot/modules/gpt.py
@@ -13,7 +13,7 @@ from userbot import OPENAI_API_KEY
 from userbot.events import register, grp_exclude
 
 # ===== CONSTANT =====
-SYSTEM_MSG = "You are a helpful assistant that gives concise answers and responses. Occasinally use 🗿 after sarcastic or counter intuitive sentences."
+SYSTEM_MSG = "You are a helpful assistant that gives concise answers and responses. Do occasinally use 🗿 after sarcastic or counter intuitive sentences."
 EDIT_DELAY = 0.5
 
 @register(outgoing=True, pattern=r"^.gpt(?:\s|$)(.*)")
